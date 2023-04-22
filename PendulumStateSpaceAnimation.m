5e887a1927f4f465bcc3807ae2da539e5453161d
3LucasZ <lucas.zheng@warriorlife.net>
2023-04-21 18:11:53 -0700

    Update PendulumStateSpaceAnimation.m

--- PendulumStateSpaceAnimation.m
+++ PendulumStateSpaceAnimation.m
@@ -20,21 +20,36 @@ for i = 1:numel(x)
 end
 
 % display the vector field
-quiver(x,y,u,v,'r'); figure(gcf)
+quiver(x,y,u,v,'r');
+field = figure(gcf)
 xlabel('angle (theta)')
 ylabel('angular velocity (w)')
 axis tight equal;
 
-% initial conditions
+% param
 theta = 1;
 w = 0;
+tstep=0.05;
+tspan = 0:tstep:3;
 
-% graph the solved equation
-tspan = [0,10]
+% animate
+function myfun(src,event)
+  disp(event.Key);
+end
+set(field,'KeyPressFcn',@myfun);
+
 hold on
-[ts,ys] = ode45(f,tspan,[theta;w]);
-plot(ys(:,1),ys(:,2))
-plot(ys(1,1),ys(1,2),'bo') % plot starting point as blue circle
-plot(ys(end,1),ys(end,2),'ks') % plot ending point as black square
+for tcur = tspan
+  point = plot(theta,w,'bo');
+
+  pause(tstep);
+  [ts, ys] = ode45(f,[0,tstep],[theta;w]);
+  theta = ys(end,1)
+  w = ys(end,2)
+
+  delete(point);
+end
 hold off
+
+
 
